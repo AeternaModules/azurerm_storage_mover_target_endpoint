4c28390922165f66d850e3f0dbb006d3ecc46553
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:51 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "storage_mover_target_endpoints" {
-  description = "All storage_mover_target_endpoint resources"
-  value       = azurerm_storage_mover_target_endpoint.storage_mover_target_endpoints
-}
 output "storage_mover_target_endpoints_description" {
-  description = "List of description values across all storage_mover_target_endpoints"
-  value       = [for k, v in azurerm_storage_mover_target_endpoint.storage_mover_target_endpoints : v.description]
+  description = "Map of description values across all storage_mover_target_endpoints, keyed the same as var.storage_mover_target_endpoints"
+  value       = { for k, v in azurerm_storage_mover_target_endpoint.storage_mover_target_endpoints : k => v.description }
 }
 output "storage_mover_target_endpoints_name" {
-  description = "List of name values across all storage_mover_target_endpoints"
-  value       = [for k, v in azurerm_storage_mover_target_endpoint.storage_mover_target_endpoints : v.name]
+  description = "Map of name values across all storage_mover_target_endpoints, keyed the same as var.storage_mover_target_endpoints"
+  value       = { for k, v in azurerm_storage_mover_target_endpoint.storage_mover_target_endpoints : k => v.name }
 }
 output "storage_mover_target_endpoints_storage_account_id" {
-  description = "List of storage_account_id values across all storage_mover_target_endpoints"
-  value       = [for k, v in azurerm_storage_mover_target_endpoint.storage_mover_target_endpoints : v.storage_account_id]
+  description = "Map of storage_account_id values across all storage_mover_target_endpoints, keyed the same as var.storage_mover_target_endpoints"
+  value       = { for k, v in azurerm_storage_mover_target_endpoint.storage_mover_target_endpoints : k => v.storage_account_id }
 }
 output "storage_mover_target_endpoints_storage_container_name" {
-  description = "List of storage_container_name values across all storage_mover_target_endpoints"
-  value       = [for k, v in azurerm_storage_mover_target_endpoint.storage_mover_target_endpoints : v.storage_container_name]
+  description = "Map of storage_container_name values across all storage_mover_target_endpoints, keyed the same as var.storage_mover_target_endpoints"
+  value       = { for k, v in azurerm_storage_mover_target_endpoint.storage_mover_target_endpoints : k => v.storage_container_name }
 }
 output "storage_mover_target_endpoints_storage_mover_id" {
-  description = "List of storage_mover_id values across all storage_mover_target_endpoints"
-  value       = [for k, v in azurerm_storage_mover_target_endpoint.storage_mover_target_endpoints : v.storage_mover_id]
+  description = "Map of storage_mover_id values across all storage_mover_target_endpoints, keyed the same as var.storage_mover_target_endpoints"
+  value       = { for k, v in azurerm_storage_mover_target_endpoint.storage_mover_target_endpoints : k => v.storage_mover_id }
 }
 
