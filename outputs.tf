e30859d74d000af14e1718d246cbf178a4303dfb
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:32:03 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "storage_mover_target_endpoints_id" {
+  description = "Map of id values across all storage_mover_target_endpoints, keyed the same as var.storage_mover_target_endpoints"
+  value       = { for k, v in azurerm_storage_mover_target_endpoint.storage_mover_target_endpoints : k => v.id }
+}
 output "storage_mover_target_endpoints_description" {
   description = "Map of description values across all storage_mover_target_endpoints, keyed the same as var.storage_mover_target_endpoints"
   value       = { for k, v in azurerm_storage_mover_target_endpoint.storage_mover_target_endpoints : k => v.description }
